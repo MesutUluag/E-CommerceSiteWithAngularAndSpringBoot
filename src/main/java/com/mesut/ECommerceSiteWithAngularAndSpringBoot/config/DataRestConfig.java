e4d8d9d663de88ea727a9488db34d13a116d914e
Mesut Uluag <punk7119@gmail.com>
2020-11-26 10:03:48 +0300

    configure rest apis only readable

---
+++ src/main/java/com/mesut/ECommerceSiteWithAngularAndSpringBoot/config/DataRestConfig.java
@@ -0,0 +1,29 @@
+package com.mesut.ECommerceSiteWithAngularAndSpringBoot.config;
+
+import com.mesut.ECommerceSiteWithAngularAndSpringBoot.entity.Product;
+import com.mesut.ECommerceSiteWithAngularAndSpringBoot.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions={HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory .class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
+
+
+}
+}
