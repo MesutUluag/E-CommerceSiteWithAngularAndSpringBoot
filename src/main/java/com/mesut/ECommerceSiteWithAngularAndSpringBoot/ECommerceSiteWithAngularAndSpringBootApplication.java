30f0b0ef3b0c9ff4b705ea718f75d4dcd6668628
Mesut Uluag <punk7119@gmail.com>
2020-11-26 07:54:28 +0300

    Initial commit

---
+++ src/main/java/com/mesut/ECommerceSiteWithAngularAndSpringBoot/ECommerceSiteWithAngularAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mesut.ECommerceSiteWithAngularAndSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSiteWithAngularAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSiteWithAngularAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesut/ECommerceSiteWithAngularAndSpringBoot/ECommerceSiteWithAngularAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesut.ECommerceSiteWithAngularAndSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteWithAngularAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
