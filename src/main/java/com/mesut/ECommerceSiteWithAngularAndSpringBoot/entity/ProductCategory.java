7904a03f7f366c1ca2b3a3e7fad4fd3226e4eb84
Mesut Uluag <punk7119@gmail.com>
2020-11-26 08:53:33 +0300

    create Product and ProductCategory Entities

---
+++ src/main/java/com/mesut/ECommerceSiteWithAngularAndSpringBoot/entity/ProductCategory.java
@@ -0,0 +1,27 @@
+package com.mesut.ECommerceSiteWithAngularAndSpringBoot.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+//@Data will be bug in here because of one to many relation instead we are using below shown annotations
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
