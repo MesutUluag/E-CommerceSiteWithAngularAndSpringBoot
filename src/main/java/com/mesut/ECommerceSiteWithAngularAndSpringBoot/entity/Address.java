347848b88e43543d814bbf4ca6430353c4140a5c
developer <punk7119@gmail.com>
2020-12-03 14:13:25 +0300

    create entities and relations for saving the order

---
+++ src/main/java/com/mesut/ECommerceSiteWithAngularAndSpringBoot/entity/Address.java
@@ -0,0 +1,35 @@
+package com.mesut.ECommerceSiteWithAngularAndSpringBoot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Order order;
+}
